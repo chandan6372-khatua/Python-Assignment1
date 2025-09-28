926f484e1ca6bf4a64ad3b3120da1c91b34d6f04
chandan6372-khatua <chandankumarkhatua381@gmail.com>
2025-09-28 09:20:47 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,11 @@
+# Task 2: Create a Personalized Greeting
+
+# Take first and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate into full name
+full_name = first_name + " " + last_name
+
+# Print personalized greeting
+print("Hello,", full_name + "! Welcome to Python programming.")
